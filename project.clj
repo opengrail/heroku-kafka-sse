fffe3c6d24233d85fd5c6b03aab6ab3196db9e06
Ray McDermott <ray@mcdermott.be>
2016-11-09 15:17:48 +0100

    log4j

--- project.clj
+++ project.clj
@@ -17,4 +17,5 @@
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
   :uberjar-name "clojure-getting-started-standalone.jar"
-  :profiles {:production {:env {:production true}}})
+  :profiles {:production {:env {:production true}}}
+  :pedantic? :warn)
