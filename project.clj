604b37ea6cb4a364553964ede62168ae83ee3d82
Ray McDermott <ray@mcdermott.be>
2016-11-11 21:47:33 +0100

    sse-try-10

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [ring/ring-jetty-adapter "1.4.0"]
                  [environ "1.1.0"]
                  [com.github.jkutner/env-keystore "0.1.2"]
-                 [com.opengrail/kafka-sse-clj "0.1.0"]
+                 [com.opengrail/kafka-sse-clj "0.1.1"]
                  ; only for the demo ... these libs are needed to run the Kafka Producer
                  [org.apache.kafka/kafka_2.10 "0.10.1.0"]
                  [org.apache.kafka/kafka-clients "0.10.1.0"]]
